92fa046c06496a2f444706ef1b0286bed603b153
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-07-25 19:40:07 +0200

    Use from entries

--- packages/common/src/spokenForms/characters.ts
+++ packages/common/src/spokenForms/characters.ts
@@ -4,21 +4,20 @@ import { NoSpokenFormError } from "..";
 
 // https://github.com/talonhub/community/blob/9acb6c9659bb0c9b794a7b7126d025603b4ed726/core/keys/keys.py
 
-const alphabet =
+const alphabet = Object.fromEntries(
   "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip"
     .split(" ")
-    .reduce<Record<string, string>>((result, phrase, index) => {
-      const letter = String.fromCharCode("a".charCodeAt(0) + index);
-      result[letter] = phrase;
-      return result;
-    }, {});
+    .map((word, index) => [
+      String.fromCharCode("a".charCodeAt(0) + index),
+      word,
+    ]),
+);
 
-const digits = "zero one two three four five six seven eight nine"
-  .split(" ")
-  .reduce<Record<string, string>>((result, phrase, index) => {
-    result[index.toString()] = phrase;
-    return result;
-  }, {});
+const digits = Object.fromEntries(
+  "zero one two three four five six seven eight nine"
+    .split(" ")
+    .map((word, index) => [index.toString(), word]),
+);
 
 const symbols = {
   ".": "dot",
